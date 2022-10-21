bc709e085ab652e00228e10f01d4c9a735a9473e
sunil10790play <sunil10790play@gmail.com>
2022-10-21 09:43:50 +0530

    Add User Service

---
+++ src/main/java/com/mrinterceptor/app/ws/service/UserService.java
@@ -0,0 +1,7 @@
+package com.mrinterceptor.app.ws.service;
+
+import com.mrinterceptor.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+	UserDto createUser(UserDto user);
+}
---
+++ src/main/java/com/mrinterceptor/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.mrinterceptor.app.ws.service.impl;
+
+import com.mrinterceptor.app.ws.service.UserService;
+import com.mrinterceptor.app.ws.shared.dto.UserDto;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
